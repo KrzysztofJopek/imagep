60f8906d1d1838584f77c3ee6cfa4990ef5ca097
Krzysztof <krzysztofjopek99@gmail.com>
2019-05-22 12:16:00 +0200

    Fix indentiation

--- src/filtration.c
+++ src/filtration.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 
 void get_mult_mask(struct mask mask, struct mask* values, struct image* image,
-		int x, int y, int channel);
+        int x, int y, int channel);
 
 /*
  * Apply mask to the image and return output image,
  * You have to specify filtration function as defined filt_func
  */ 
 struct image* apply_mask(struct image* image_in, struct mask mask,
-		double (*filt_func)(struct mask))
+        double (*filt_func)(struct mask))
 {
     struct image* image_out = empty_image(image_in->width, image_in->height,
             image_in->channels);
     int center = mask.size/2;
     double data[mask.size*mask.size];
     struct mask temp = {data, mask.size};
-    
-	for(int x=0; x<image_in->width - mask.size; x++){
-		for(int y=0; y<image_in->height - mask.size; y++){
-			for(int ch=0; ch<image_in->channels; ch++){
-				get_mult_mask(mask, &temp, image_in, x, y, ch);
-				*(get_pixel(image_out, x+center, y+center)+ch) = (*filt_func)(temp)*256;
+
+    for(int x=0; x<image_in->width - mask.size; x++){
+        for(int y=0; y<image_in->height - mask.size; y++){
+            for(int ch=0; ch<image_in->channels; ch++){
+                get_mult_mask(mask, &temp, image_in, x, y, ch);
+                *(get_pixel(image_out, x+center, y+center)+ch) = (*filt_func)(temp)*256;
                 //printf("%d\n", *(get_pixel(image_out, x+center, y+center)+ch));
-			}
-		}
-	}
+            }
+        }
+    }
     return image_out;
 }
 
@@ -35,15 +35,15 @@ struct image* apply_mask(struct image* image_in, struct mask mask,
  * and return output in values struct
  */
 void get_mult_mask(struct mask mask, struct mask* values, struct image* image,
-		int x, int y, int channel)
+        int x, int y, int channel)
 {
-	double* p = values->values;
-	for(int i=x; i<x+mask.size; i++){
-		for(int j=y; j<y+mask.size; j++){
-			unsigned char* pix = get_pixel(image, i, j);
-			*p++ = (double)(*(pix+channel))/256 * *mask.values++;
-		}
-	}
+    double* p = values->values;
+    for(int i=x; i<x+mask.size; i++){
+        for(int j=y; j<y+mask.size; j++){
+            unsigned char* pix = get_pixel(image, i, j);
+            *p++ = (double)(*(pix+channel))/256 * *mask.values++;
+        }
+    }
 }
 
 /*
